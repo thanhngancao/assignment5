041fb37d977c37dfd4e85714a7ad7f338cf81b8f
thanhngancao <thanhngancao83@gmail.com>
2020-05-01 13:47:22 +0700

    sss

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const hostname = 'localhost';
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
     extended: true
 }))
-app.use(express.static('public'))
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    console.log("request");
-    next();
-});
+
+// app.use(express.static('public'))
+
+// app.use((req, res, next) => {
+//     console.log("request");
+//     next();
+// });
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.render('index');
 })
 
 //require đến file ./routers/userRouter.js
@@ -23,6 +29,4 @@ var userRoute = require("./routers/usersRouter");
 //Dùng userRoute cho tất cả các route bắt đầu bằng '/users'
 app.use('/users', userRoute);
 
-app.listen(port, hostname, () => {
-    console.log(`I wil run on ${hostname}:${port}/`);
-});
+app.listen(port);
